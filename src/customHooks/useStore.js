5b9dfa2d0df343e3b7e9c535f3fac98b8927721f
fichorka <fichorka@gmail.com>
2019-11-22 14:24:50 +0100

    Refactor initialState

--- src/customHooks/useStore.js
+++ src/customHooks/useStore.js
@@ -1,5 +1,6 @@
 import { useReducer } from 'react'
 import rootReducer from '../reducers'
+import { IMAGE_CONFIG, GENRES } from '../utilities/constants.js'
 
 // useStore
 export default () => {
@@ -14,88 +15,6 @@ const initialState = {
   isLoading: true,
   movies: [],
   isLoaded: false,
-  imageConfig: {
-    baseUrl: 'https://image.tmdb.org/t/p/',
-    backdropSize: 'w1280',
-    posterSize: 'w500'
-  },
-  // consider fetching genres dynamically
-  genres: [
-    {
-      id: 28,
-      name: 'Action'
-    },
-    {
-      id: 12,
-      name: 'Adventure'
-    },
-    {
-      id: 16,
-      name: 'Animation'
-    },
-    {
-      id: 35,
-      name: 'Comedy'
-    },
-    {
-      id: 80,
-      name: 'Crime'
-    },
-    {
-      id: 99,
-      name: 'Documentary'
-    },
-    {
-      id: 18,
-      name: 'Drama'
-    },
-    {
-      id: 10751,
-      name: 'Family'
-    },
-    {
-      id: 14,
-      name: 'Fantasy'
-    },
-    {
-      id: 36,
-      name: 'History'
-    },
-    {
-      id: 27,
-      name: 'Horror'
-    },
-    {
-      id: 10402,
-      name: 'Music'
-    },
-    {
-      id: 9648,
-      name: 'Mystery'
-    },
-    {
-      id: 10749,
-      name: 'Romance'
-    },
-    {
-      id: 878,
-      name: 'Science Fiction'
-    },
-    {
-      id: 10770,
-      name: 'TV Movie'
-    },
-    {
-      id: 53,
-      name: 'Thriller'
-    },
-    {
-      id: 10752,
-      name: 'War'
-    },
-    {
-      id: 37,
-      name: 'Western'
-    }
-  ]
+  imageConfig: IMAGE_CONFIG,
+  genres: GENRES
 }
