e7f6922fa3c595156cf62345bf805cf323458f0d
fichorka <fichorka@gmail.com>
2019-11-23 20:21:22 +0100

    reconfigure webpack and remove redux dependencies

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   context: path.resolve(__dirname),
   entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, './build'),
+    path: path.resolve(__dirname, './public'),
     filename: 'main.js',
     publicPath: '/'
   },
